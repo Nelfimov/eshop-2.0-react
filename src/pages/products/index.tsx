2481ca27a2234fbf45e64e1f43fffef517a33343
nelfimov <nelfimov@mail.ru>
2023-02-16 10:48:55 +0300

    deleted bloat

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,7 +1,9 @@
 import useSWR from 'swr';
+import Head from 'next/head.js';
+import { fetcher } from '@/helpers/fetcher.js';
 
-export default function Products(): JSX.Element {
-  const { data, error } = useSWR('/products', fetch);
+export default function Products() {
+  const { data, error } = useSWR('/products', fetcher);
 
   if (error) {
     return <h1>Failed to load products</h1>;
@@ -9,5 +11,12 @@ export default function Products(): JSX.Element {
   if (!data) {
     return <h1>Loading...</h1>;
   }
-  return <h1>When nothing to be loaded</h1>;
+  return (
+    <>
+      <Head>
+        <title>Products | Jetzt ist die beste Zeit Online Shop</title>
+      </Head>
+      <h1>When nothing to be loaded</h1>
+    </>
+  );
 }
