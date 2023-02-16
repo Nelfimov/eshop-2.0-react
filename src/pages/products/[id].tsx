54d654ba3491f5f4985e8cea9cadf6c735fc299b
nelfimov <nelfimov@mail.ru>
2023-02-16 10:17:59 +0300

    removed useless styling

--- src/pages/products/[id].tsx
+++ src/pages/products/[id].tsx
@@ -1,18 +1,23 @@
 import useSWR from 'swr';
 import { useRouter } from 'next/router.js';
-import { fetcher } from '@/helpers/index.js';
+import { fetcher } from '@/helpers';
+import Head from 'next/head.js';
 
 export default function Product() {
   const router = useRouter();
+
   const { data, error } = useSWR(`/products/${router.query.id}`, fetcher);
-
   if (error) return <h1>Cannot load product</h1>;
-
   if (!data) return <h1>Loading...</h1>;
 
   return (
-    <div className="Product">
-      <div className="title">{data.name}</div>
-    </div>
+    <>
+      <Head>
+        <title>{`${data.name} | Jetzt ist die beste Zeit Online Shop`}</title>
+      </Head>
+      <div className="Product">
+        <div className="title">{data.name}</div>
+      </div>
+    </>
   );
 }
