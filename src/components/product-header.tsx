34fdffdfa738af132432833b74088ec76a8963a4
nelfimov <nelfimov@mail.ru>
2023-02-17 16:15:26 +0300

    added product header component

---
+++ src/components/product-header.tsx
@@ -0,0 +1,53 @@
+import Image from 'next/image';
+import {
+  MDBCardHeader,
+  MDBRow,
+  MDBCol,
+  MDBCardTitle,
+  MDBTypography,
+  MDBCardText,
+  MDBBtn,
+  MDBIcon,
+} from 'mdb-react-ui-kit';
+import { Product as IProduct } from 'types';
+
+interface Props {
+  product: IProduct;
+}
+
+export function ProductHeader({ product }: Props) {
+  return (
+    <MDBCardHeader>
+      <MDBRow>
+        <MDBCol>
+          <Image
+            src={product.titleImage}
+            width={300}
+            height={500}
+            alt={`${product.name}-title-image`}
+          />
+        </MDBCol>
+        <MDBCol size="md" className="d-flex flex-column justify-content-center">
+          <MDBCardTitle>{product.name}</MDBCardTitle>
+          {product.discount > 0 ? (
+            <>
+              <MDBCardTitle>
+                <s>€{(product.price + product.deliveryPrice).toFixed(2)}</s>
+                <MDBTypography color="danger">
+                  €{product.totalPrice.toFixed(2)}
+                </MDBTypography>
+              </MDBCardTitle>
+            </>
+          ) : (
+            <MDBCardTitle>€{product.totalPrice.toFixed(2)}</MDBCardTitle>
+          )}
+          <MDBCardText>{product.description}</MDBCardText>
+          <MDBBtn className="m-1" color="success">
+            <MDBIcon className="me-1" icon="cart-plus" />
+            Add to cart
+          </MDBBtn>
+        </MDBCol>
+      </MDBRow>
+    </MDBCardHeader>
+  );
+}
