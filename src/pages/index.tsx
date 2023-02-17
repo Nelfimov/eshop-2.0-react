bd1f616ee7dbddf2a0ba71c6c628dad7ea051adc
nelfimov <nelfimov@mail.ru>
2023-02-17 11:09:23 +0300

    fixed home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { MDBCarousel, MDBCarouselItem } from 'mdb-react-ui-kit';
 import Head from 'next/head';
 
 export default function Home() {
@@ -9,7 +10,33 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main></main>
+      <MDBCarousel showControls showIndicators className="rounded-5">
+        <MDBCarouselItem
+          active
+          itemId={1}
+          className="w-100 d-block"
+          src="https://mdbootstrap.com/img/new/slides/041.jpg"
+        >
+          <h5>First slide</h5>
+          <p>Welcome to our shop</p>
+        </MDBCarouselItem>
+        <MDBCarouselItem
+          className="w-100 d-block"
+          itemId={2}
+          src="https://mdbootstrap.com/img/new/slides/042.jpg"
+        >
+          <h5>Second slide</h5>
+          <p>Welcome again</p>
+        </MDBCarouselItem>
+        <MDBCarouselItem
+          className="w-100 d-block"
+          itemId={3}
+          src="https://mdbootstrap.com/img/new/slides/043.jpg"
+        >
+          <h5>Third slide</h5>
+          <p>Welcome again again</p>
+        </MDBCarouselItem>
+      </MDBCarousel>
     </>
   );
 }
