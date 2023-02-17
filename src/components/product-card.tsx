d7ab175a68f3832111241871ab73b679391e4948
nelfimov <nelfimov@mail.ru>
2023-02-17 10:04:38 +0300

    added product page

--- src/components/product-card.tsx
+++ src/components/product-card.tsx
@@ -3,21 +3,50 @@ import {
   MDBCardBody,
   MDBCardImage,
   MDBCardTitle,
+  MDBTypography,
 } from 'mdb-react-ui-kit';
+import Image from 'next/image';
 import Link from 'next/link';
+import { Product } from 'types';
 
-export function ProductCard({ product }: any) {
+interface Props {
+  product: Product;
+}
+
+export function ProductCard({ product }: Props) {
   return (
-    <MDBCard className="m-0 h-100">
-      <Link href={`/products/${product._id}`}>
+    <MDBCard className="p-0">
+      <Link
+        className="card-link"
+        href={`/products/${encodeURIComponent(product._id)}/?name=${
+          product.name
+        }`}
+      >
+        <Image
+          className="card-img-top"
+          src={product.titleImage}
+          alt={`${product.name}-title-image`}
+          width={200}
+          height={350}
+        />
         <MDBCardBody>
-          <MDBCardImage
-            src={product.titleImage}
-            alt={`${product.name}-title-image`}
-            position="top"
-          />
           <MDBCardTitle>{product.name}</MDBCardTitle>
-          <MDBCardTitle>{product.price}</MDBCardTitle>
+          {product.deliveryPrice > 0 ? (
+            <>
+              <MDBCardTitle>
+                <MDBTypography tag="s">
+                  €{(product.price + product.deliveryPrice).toFixed(2)}
+                </MDBTypography>
+              </MDBCardTitle>
+              <MDBCardTitle style={{ color: 'red' }}>
+                <MDBTypography tag="h4">
+                  €{product.totalPrice.toFixed(2)}
+                </MDBTypography>
+              </MDBCardTitle>
+            </>
+          ) : (
+            <MDBCardTitle>€{product.totalPrice.toFixed(2)}</MDBCardTitle>
+          )}
         </MDBCardBody>
       </Link>
     </MDBCard>
--- src/pages/products/[id].tsx
+++ src/pages/products/[id].tsx
@@ -1,19 +1,97 @@
 import useSWR from 'swr';
+import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { fetcher } from '@/helpers';
 import Head from 'next/head';
-import { MDBCard } from 'mdb-react-ui-kit';
+import {
+  MDBBtn,
+  MDBCard,
+  MDBCardBody,
+  MDBCardHeader,
+  MDBCardText,
+  MDBCardTitle,
+  MDBCol,
+  MDBRow,
+  MDBTypography,
+} from 'mdb-react-ui-kit';
+import { Product as IProduct } from 'types';
 
 export default function Product() {
   const router = useRouter();
-  const { data, error } = useSWR(`/products/${router.query.id}`, fetcher);
+  const { data, error } = useSWR(
+    `http://localhost:3001/products/${router.query.id}`,
+    fetcher
+  );
+  const product: IProduct = data && data.product;
 
   return (
     <>
       <Head>
-        <title>{`${data.product.name} | Jetzt ist die beste Zeit Online Shop`}</title>
+        <title>{`${router.query.name} | Jetzt ist die beste Zeit Online Shop`}</title>
       </Head>
-      <MDBCard></MDBCard>
+      <MDBCard>
+        {error && <h1>Product not found</h1>}
+        {!product ? (
+          <h1>Loading...</h1>
+        ) : (
+          <>
+            <MDBCardHeader>
+              <MDBRow>
+                <MDBCol>
+                  <Image
+                    src={product.titleImage}
+                    width={300}
+                    height={500}
+                    alt={`${product.name}-title-image`}
+                  />
+                </MDBCol>
+                <MDBCol
+                  size="md"
+                  className="d-flex flex-column justify-content-center"
+                >
+                  <MDBCardTitle>{product.name}</MDBCardTitle>
+                  {product.discount > 0 ? (
+                    <>
+                      <MDBCardTitle>
+                        <s>
+                          €{(product.price + product.deliveryPrice).toFixed(2)}
+                        </s>
+                        <MDBTypography color="danger">
+                          €{product.totalPrice.toFixed(2)}
+                        </MDBTypography>
+                      </MDBCardTitle>
+                    </>
+                  ) : (
+                    <MDBCardTitle>
+                      €{product.totalPrice.toFixed(2)}
+                    </MDBCardTitle>
+                  )}
+                  <MDBCardText>{product.description}</MDBCardText>
+                  <MDBBtn className="m-1" color="success">
+                    Add to cart
+                  </MDBBtn>
+                </MDBCol>
+              </MDBRow>
+            </MDBCardHeader>
+            <MDBCardBody>
+              <MDBRow>
+                {product.subImages.map((image, index) => {
+                  return (
+                    <MDBCol size="sm" key={index}>
+                      <Image
+                        src={image}
+                        alt={`${product.name}-sub-image-${index}`}
+                        width={300}
+                        height={500}
+                      />
+                    </MDBCol>
+                  );
+                })}
+              </MDBRow>
+            </MDBCardBody>
+          </>
+        )}
+      </MDBCard>
     </>
   );
 }
