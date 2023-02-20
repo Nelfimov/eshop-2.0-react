0fbfe4154bd04875020985b27311d173be14c770
nelfimov <nelfimov@mail.ru>
2023-02-20 12:11:01 +0300

    added auth page

---
+++ src/pages/auth/index.tsx
@@ -0,0 +1,32 @@
+import { MDBBtn, MDBInput } from 'mdb-react-ui-kit';
+
+export default function Auth() {
+  return (
+    <form className="mx-auto lg" style={{ maxWidth: '600px' }}>
+      <MDBInput
+        className="mb-4"
+        type="email"
+        name="email"
+        id="email"
+        label="Email address"
+      />
+      <MDBInput
+        className="mb-4"
+        type="text"
+        name="password"
+        id="password"
+        label="Password"
+      />
+      <MDBInput
+        className="mb-4"
+        type="text"
+        name="password-confirm"
+        id="password-confirm"
+        label="Confirm password"
+      />
+      <MDBBtn className="w-100" type="submit">
+        Log in
+      </MDBBtn>
+    </form>
+  );
+}
