261e7dd61ee6273c5786fc0a3604a5660072549c
nelfimov <nelfimov@mail.ru>
2023-02-21 15:31:13 +0300

    added context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,14 @@ import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import type { AppProps } from 'next/app';
 import { Layout } from '@/components';
+import { CartProvider } from '@/context';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <CartProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </CartProvider>
   );
 }
