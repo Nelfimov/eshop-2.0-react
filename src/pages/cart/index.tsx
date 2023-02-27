698af961ed8d82102bd54c28706f6fc7b2a391dd
nelfimov <nelfimov@mail.ru>
2023-02-27 12:26:49 +0300

    fixed styling

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -28,6 +28,7 @@ export default function Cart() {
 
   useEffect(() => {
     !isLoading && cart!.updatePrices(data.products);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   if (error)
@@ -49,74 +50,82 @@ export default function Cart() {
         <MDBCardHeader>
           <h1>Cart</h1>
         </MDBCardHeader>
-        <MDBCardBody>
-          {isLoading || isValidating ? (
-            <Loader />
-          ) : (
-            <MDBTable responsive="sm" striped hover>
-              <MDBTableHead>
-                <tr>
-                  <th style={{ textAlign: 'center' }} scope="col">
-                    #
-                  </th>
-                  <th style={{ textAlign: 'center' }} scope="col">
-                    Name
-                  </th>
-                  <th style={{ textAlign: 'center' }} scope="col">
-                    Quantity
-                  </th>
-                  <th style={{ textAlign: 'center' }} scope="col">
-                    Price
-                  </th>
-                  <th style={{ textAlign: 'center' }} scope="col">
-                    Total amount
-                  </th>
-                  <th style={{ textAlign: 'center' }} scope="col">
-                    Delete?
-                  </th>
-                </tr>
-              </MDBTableHead>
-              <MDBTableBody>
-                {data &&
-                  data.products.map((product: Product, index: number) => (
-                    <ItemList
-                      key={product._id}
-                      product={product}
-                      index={index + 1}
-                      quantity={
-                        // @ts-expect-error: ignore
-                        cart!.cartItems.find(
-                          (item: CartItem) => item.id === product._id
-                        ).quantity
-                      }
-                    />
-                  ))}
-              </MDBTableBody>
-              <tfoot>
-                <tr className="table-info">
-                  <td colSpan={5} align="right">
-                    TOTAL:
-                  </td>
-                  <td align="center">€{cart?.total}</td>
-                </tr>
-              </tfoot>
-            </MDBTable>
-          )}
-        </MDBCardBody>
-        <MDBCardFooter>
-          <MDBRow className="row-cols-1 row-cols-sm-2 g-4">
-            <Link href="/" className="d-flex">
-              <MDBBtn className="flex-fill" color="secondary">
-                Continue shopping
-              </MDBBtn>
-            </Link>
-            <Link href="/cart/checkout" className="d-flex">
-              <MDBBtn color="success" className="flex-fill">
-                To checkout
-              </MDBBtn>
-            </Link>
-          </MDBRow>
-        </MDBCardFooter>
+        {cart!.itemCount === 0 ? (
+          <MDBCardBody>
+            <h4 style={{ textAlign: 'center' }}>Empty</h4>
+          </MDBCardBody>
+        ) : (
+          <MDBCardBody>
+            {isLoading || isValidating ? (
+              <Loader />
+            ) : (
+              <MDBTable responsive="sm" striped hover>
+                <MDBTableHead>
+                  <tr>
+                    <th style={{ textAlign: 'center' }} scope="col">
+                      #
+                    </th>
+                    <th style={{ textAlign: 'center' }} scope="col">
+                      Name
+                    </th>
+                    <th style={{ textAlign: 'center' }} scope="col">
+                      Quantity
+                    </th>
+                    <th style={{ textAlign: 'center' }} scope="col">
+                      Price
+                    </th>
+                    <th style={{ textAlign: 'center' }} scope="col">
+                      Total amount
+                    </th>
+                    <th style={{ textAlign: 'center' }} scope="col">
+                      Delete?
+                    </th>
+                  </tr>
+                </MDBTableHead>
+                <MDBTableBody>
+                  {data &&
+                    data.products.map((product: Product, index: number) => (
+                      <ItemList
+                        key={product._id}
+                        product={product}
+                        index={index + 1}
+                        quantity={
+                          // @ts-expect-error: ignore
+                          cart!.cartItems.find(
+                            (item: CartItem) => item.id === product._id
+                          ).quantity
+                        }
+                      />
+                    ))}
+                </MDBTableBody>
+                <tfoot>
+                  <tr className="table-info">
+                    <td colSpan={5} align="right">
+                      TOTAL:
+                    </td>
+                    <td align="center">€{cart?.total}</td>
+                  </tr>
+                </tfoot>
+              </MDBTable>
+            )}
+          </MDBCardBody>
+        )}
+        {cart!.itemCount > 0 && (
+          <MDBCardFooter>
+            <MDBRow className="row-cols-1 row-cols-sm-2 g-4">
+              <Link href="/" className="d-flex">
+                <MDBBtn className="flex-fill" color="secondary">
+                  Continue shopping
+                </MDBBtn>
+              </Link>
+              <Link href="/cart/checkout" className="d-flex">
+                <MDBBtn color="success" className="flex-fill">
+                  To checkout
+                </MDBBtn>
+              </Link>
+            </MDBRow>
+          </MDBCardFooter>
+        )}
       </MDBCard>
     </>
   );
