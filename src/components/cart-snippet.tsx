96d32719ab7bd895912649ddceb2018362584807
Nelfimov <nelfimov@mail.ru>
2023-03-23 10:10:37 +0300

    switched to cart context calculus

--- src/components/cart-snippet.tsx
+++ src/components/cart-snippet.tsx
@@ -20,18 +20,6 @@ interface Props {
 }
 
 export function CartSnippet({ products, cart }: Props) {
-  const totalShipping = products.reduce((total, current) => {
-    const q = cart.cartItems.find((item) => item.id === current.id)?.quantity;
-    return total + current.deliveryPrice * (q ?? 0);
-  }, 0);
-  const totalItems = products.reduce((total, current) => {
-    const q = cart.cartItems.find((item) => item.id === current.id)?.quantity;
-    return total + current.price * (q ?? 0);
-  }, 0);
-  const totalDiscount = products.reduce((total, current) => {
-    const q = cart.cartItems.find((item) => item.id === current.id)?.quantity;
-    return total + current.discount * (q ?? 0);
-  }, 0);
   return (
     <MDBCard>
       <MDBCardHeader className="d-flex justify-content-between align-items-center">
@@ -74,7 +62,7 @@ export function CartSnippet({ products, cart }: Props) {
             <h6 className="mb-0">Total items:</h6>
           </MDBCol>
           <MDBCol align="right">
-            <h6 className="mb-0">{formatAsPrice(totalItems)}</h6>
+            <h6 className="mb-0">{formatAsPrice(cart.totalItems)}</h6>
           </MDBCol>
         </MDBRow>
       </MDBCardFooter>
@@ -84,7 +72,7 @@ export function CartSnippet({ products, cart }: Props) {
             <h6 className="mb-0">Total shipping:</h6>
           </MDBCol>
           <MDBCol align="right">
-            <h6 className="mb-0">+ {formatAsPrice(totalShipping)}</h6>
+            <h6 className="mb-0">+ {formatAsPrice(cart.totalShippings)}</h6>
           </MDBCol>
         </MDBRow>
       </MDBCardFooter>
@@ -94,7 +82,7 @@ export function CartSnippet({ products, cart }: Props) {
             <h6 className="mb-0">Total discount:</h6>
           </MDBCol>
           <MDBCol align="right">
-            <h6 className="mb-0">- {formatAsPrice(totalDiscount)}</h6>
+            <h6 className="mb-0">- {formatAsPrice(cart.totalDiscounts)}</h6>
           </MDBCol>
         </MDBRow>
       </MDBCardFooter>
@@ -104,7 +92,7 @@ export function CartSnippet({ products, cart }: Props) {
             <h5>Total:</h5>
           </MDBCol>
           <MDBCol align="right">
-            <h5>{formatAsPrice(cart.total)}</h5>
+            <h5>{formatAsPrice(cart.totalCart)}</h5>
           </MDBCol>
         </MDBRow>
       </MDBCardFooter>
